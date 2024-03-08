9d39349894662c21c1f90208b71bfbb2395755c3
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2024-03-08 14:41:03 +0600

    updated

--- src/main/java/com/template/vaxGuard/models/pendingVaccineCandidateFromHospital.java
+++ src/main/java/com/template/vaxGuard/models/pendingVaccineCandidateFromHospital.java
@@ -6,7 +6,12 @@ import java.util.List;
 
 public class pendingVaccineCandidateFromHospital extends vaccineCandidate{
 
-    public pendingVaccineCandidateFromHospital(String email, String role, String password, String babyName, Date birthDate, Time birthTime, String birthHospitalName, String birthID, List<userTakenVaccines> takenVaccinesList, List<userPendingVaccines> pendingVaccinesList) {
-        super(email, role, password, babyName, birthDate, birthTime, birthHospitalName, birthID, takenVaccinesList, pendingVaccinesList);
+
+    public pendingVaccineCandidateFromHospital(String babyName, String email, Date birthDate, Time birthTime, String birthHospitalName) {
+        super.setBabyName(babyName);
+        super.setEmail(email);
+        super.setBirthDate(birthDate);
+        super.setBirthTime(birthTime);
+        super.setBirthHospitalName(birthHospitalName);
     }
 }
