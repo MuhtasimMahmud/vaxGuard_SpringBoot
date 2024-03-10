99bc9fd3bd070d7484b908b64edb629334fa1f2c
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2024-03-11 00:39:35 +0600

    updated

--- src/main/java/com/template/vaxGuard/controller/HospitalController.java
+++ src/main/java/com/template/vaxGuard/controller/HospitalController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 
@@ -30,7 +32,7 @@ public class HospitalController {
         return "GovtHospitalORhealthCareAuthority/childBirthRegistration";
     }
 
-    @GetMapping("do_pendingCandidateRegistration")
+    @RequestMapping(value = "do_pendingCandidateRegistration", method = RequestMethod.POST)
     public String do_pendingCandidateRegistration(@ModelAttribute("pendingCandidateObject")pendingCandidateFromHospital pendingCandidate, Model model, HttpSession session){
 
         try{
