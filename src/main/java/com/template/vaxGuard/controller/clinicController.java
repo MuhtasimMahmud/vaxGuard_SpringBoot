3e8a604463db0653cd187bfe5cfbeb89acf98adb
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2024-03-17 17:25:36 +0600

    updated

--- src/main/java/com/template/vaxGuard/controller/clinicController.java
+++ src/main/java/com/template/vaxGuard/controller/clinicController.java
@@ -1,14 +1,21 @@
 package com.template.vaxGuard.controller;
 
+import com.template.vaxGuard.helper.Message;
+import com.template.vaxGuard.models.User;
 import com.template.vaxGuard.models.clinic;
+import com.template.vaxGuard.repositories.UserRepository;
 import com.template.vaxGuard.repositories.clinicRepository;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.Method;
 import java.security.Principal;
 
 @Controller
@@ -18,8 +25,13 @@ public class clinicController {
     @Autowired
     clinicRepository clinicRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     public clinic currentLoggedInClinic(Principal principal){
         String username = principal.getName();
+        User user = userRepository.findByEmail(username);
+
         clinic clinic = clinicRepository.findByEmail(username);
 
         return clinic;
@@ -57,6 +69,28 @@ public class clinicController {
         model.addAttribute("clinic", clinic);
 
         return "vaccineGivingClinic/profile";
+    }
+
+    @GetMapping("/updateClinicProfile")
+    public String updateClinicProfile(@ModelAttribute("clinic")clinic clinic, Model model, Principal principal, HttpSession session){
+
+        clinic existingClinic = clinicRepository.findByEmail(currentLoggedInClinic(principal).getEmail());
+
+
+        try{
+            if(existingClinic != null){
+                existingClinic.setAddress(clinic.getAddress());
+                clinicRepository.save(existingClinic);
+                model.addAttribute("clinic", existingClinic);
+                session.setAttribute("message", new Message("Your Profile is Updated", "alert-success"));
+
+            }
+        }catch (Exception exception){
+            exception.printStackTrace();
+            session.setAttribute("message", new Message("Sorry! Can't update your profile"+exception.getMessage(), "alert-danger"));
+        }
+
+        return "redirect:/clinicAuthority/clinicProfile";
     }
 
 }
