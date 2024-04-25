10812933366ddfb24bdbd84d99be998b82587660
muhtasimmahmud <muhtasim.mzs16@gmail.com>
2024-04-25 10:43:15 +0600

    updated

---
+++ src/main/resources/static/js/passwordShow.js
@@ -0,0 +1,14 @@
+document.getElementById('togglePassword').addEventListener('click', function() {
+    var passwordInput = document.getElementById('password');
+    var icon = document.getElementById('togglePassword');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.classList.remove('bi-eye-slash');
+        icon.classList.add('bi-eye');
+    } else {
+        passwordInput.type = 'password';
+        icon.classList.remove('bi-eye');
+        icon.classList.add('bi-eye-slash');
+    }
+});
